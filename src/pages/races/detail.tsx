1d006a9bfe3d74780c63b251942b9b4ca1bea48d
johnny1204 <major@za2.so-net.ne.jp>
2022-11-16 23:11:47 +0900

    feat: jsonの構造を変更したのでコンポーネントを対応

--- src/pages/races/detail.tsx
+++ src/pages/races/detail.tsx
@@ -1,40 +1,56 @@
-import React, { ChangeEvent, Fragment, useState } from 'react';
+import React, { ChangeEvent, Fragment, useEffect, useState } from 'react';
 import { Box } from '@mui/system';
 import { GetStaticProps, NextPage } from 'next';
-import { TabContext, TabPanel } from '@material-ui/lab';
+import { TabContext } from '@material-ui/lab';
 import { Tab } from '@mui/material';
 import { Tabs } from '@material-ui/core';
 import { RaceContent } from '@/components/Race/RaceContent';
 import { PlaceType, RaceContentType } from '@/types/Race';
 import { GetRaceNumbers } from '@/utils/getRaceNumbers';
 
+type PillarContentType = {
+  raceNum: number
+  horses: RaceContentType[]
+}
+
+type HorsePillarType = {
+  place: number,
+  pillars: PillarContentType[]
+}
+
 type SSGProps = {
-  rows: RaceContentType[];
-  places: PlaceType[];
+  rows: HorsePillarType[]
+  places: PlaceType[]
 }
 
 const FirstPost: NextPage<SSGProps> = ({ rows, places }) => {
-  const [value, setValue] = useState<string>(places[0].id.toString());
-  const [verticalValue, setVerticalValue] = useState('1');
+  const [placeId, setPlaceId] = useState<number>(places[0].id);
+  const [raceNum, setRaceNum] = useState<number>(1);
+
+  const [pillar, setPillar] = useState<PillarContentType>();
+
+  useEffect(() => {
+    const target = rows.find((p: HorsePillarType) => p.place === placeId);
+  
+    setPillar(target?.pillars.find((content: PillarContentType) => content.raceNum === raceNum))
+  }, [placeId, raceNum])
 
   return (
     <Box sx={{ width: '100%', paddingTop: '10px' }}>
-      <TabContext value={value}>
+      <TabContext value={placeId.toString()}>
         <Box sx={{ bgcolor: 'background.paper' }}>
           <Tabs
-            value={value}
-            onChange={(_: ChangeEvent<{}>, newValue: string) =>
-              setValue(newValue)
-            }
+            value={placeId}
+            onChange={(_: ChangeEvent<{}>, newValue: string) => setPlaceId(Number(newValue))}
             variant="fullWidth"
             indicatorColor="primary"
           >
             {
-              places.map((place: PlaceType, i: number) => (
+              places.map((place: PlaceType) => (
                   <Tab
                     key={place.id}
                     label={place.name}
-                    value={place.id.toString()}
+                    value={place.id}
                     {...{
                       id: `full-width-tab-${place.id}`,
                       'aria-controls': `full-width-tabpanel-${place.id}`
@@ -47,25 +63,25 @@ const FirstPost: NextPage<SSGProps> = ({ rows, places }) => {
         <Box>
           <Box display="flex" justifyContent="center" width="100%">
             <Tabs
-              value={verticalValue}
-              onChange={(_: ChangeEvent<{}>, newValue: string) =>
-                  setVerticalValue(newValue)
-              }
+              value={raceNum}
+              onChange={(_: ChangeEvent<{}>, newValue: string) => setRaceNum(Number(newValue))}
               variant="scrollable"
               scrollButtons="auto"
               indicatorColor="primary"
             >
-              {GetRaceNumbers.map((raceNumber, i) => (
+              {GetRaceNumbers.map((raceNumber: string, i: number) => (
                 <Tab
                   label={raceNumber}
-                  value={(i + 1).toString()}
+                  value={(i + 1)}
                   key={i}
                 />
               ))}
             </Tabs>
           </Box>
           <Box sx={{ marginTop: "10px", paddingX: "20px" }}>
-            <RaceContent props={rows} />
+            {
+              pillar && <RaceContent props={ pillar.horses} />
+            }
           </Box>
         </Box>
       </TabContext>
@@ -73,19 +89,29 @@ const FirstPost: NextPage<SSGProps> = ({ rows, places }) => {
   );
 };
 
-export const getStaticProps: GetStaticProps<SSGProps> = async (context) => {
+export const getStaticProps: GetStaticProps<SSGProps> = async () => {
   const json = JSON.stringify([
-    { num: '1', name: '馬名1', mark: null, aite: null, ana: null, win: 33 },
-    { num: '2', name: '馬名2', mark: '△', aite: null, ana: null, win: 85 },
-    { num: '3', name: '馬名3', mark: null, aite: null, ana: null, win: 66 },
-    { num: '4', name: '馬名4', mark: null, aite: null, ana: null, win: 45 },
-    { num: '5', name: '馬名5', mark: '◎', aite: null, ana: null, win: 70 },
-    { num: '6', name: '馬名6', mark: null, aite: null, ana: null, win: 13 },
-    { num: '7', name: '馬名7', mark: '◯', aite: null, ana: null, win: 11 },
-    { num: '8', name: '馬名8', mark: null, aite: null, ana: null, win: 58 },
-    { num: '9', name: '馬名9', mark: '▲', aite: null, ana: null, win: 36 },
-    { num: '10',name:  '馬名10', mark: null, aite: null, ana: null, win: 1 },
-    { num: '11',name:  '馬名11', mark: null, aite: null, ana: null, win: 1 }
+    {
+      place: 3,
+      pillars: [
+        { 
+          raceNum: 1,
+          horses: [
+            { num: '1', name: '馬名1', mark: null, aite: null, ana: null, win: 33 },
+            { num: '2', name: '馬名2', mark: '△', aite: null, ana: null, win: 85 },
+            { num: '3', name: '馬名3', mark: null, aite: null, ana: null, win: 66 },
+            { num: '4', name: '馬名4', mark: null, aite: null, ana: null, win: 45 },
+            { num: '5', name: '馬名5', mark: '◎', aite: null, ana: null, win: 70 },
+            { num: '6', name: '馬名6', mark: null, aite: null, ana: null, win: 13 },
+            { num: '7', name: '馬名7', mark: '◯', aite: null, ana: null, win: 11 },
+            { num: '8', name: '馬名8', mark: null, aite: null, ana: null, win: 58 },
+            { num: '9', name: '馬名9', mark: '▲', aite: null, ana: null, win: 36 },
+            { num: '10',name:  '馬名10', mark: null, aite: null, ana: null, win: 1 },
+            { num: '11',name:  '馬名11', mark: null, aite: null, ana: null, win: 1 }
+          ]
+        }
+      ]
+    }
   ]);
 
   const placeJson = JSON.stringify([
@@ -96,7 +122,7 @@ export const getStaticProps: GetStaticProps<SSGProps> = async (context) => {
 
   return {
     props: {
-      rows: JSON.parse(json) as RaceContentType[],
+      rows: JSON.parse(json) as HorsePillarType[],
       places: JSON.parse(placeJson) as PlaceType[]
     }
   }
